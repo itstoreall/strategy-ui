a3435487e7f3dfbf0dce5850c301e94f5c134a0c
Serhii Stanislav <serhiistanislav@gmail.com>
2024-10-21 22:23:39 +0300

    ADD | email message

--- src/lib/auth/authConfig.ts
+++ src/lib/auth/authConfig.ts
@@ -4,9 +4,28 @@ import Google from "next-auth/providers/google";
 import Nodemailer from "next-auth/providers/nodemailer";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/src/lib/prisma/client";
-// import PostgresAdapter from "@auth/pg-adapter";
-// import { pool } from "@/src/lib/postgress";
 import { clearExpiredVerificationTokens } from "@/src/lib/auth/clearStaleTokensServerAction";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER_HOST,
+  port: parseInt(process.env.EMAIL_SERVER_PORT!, 10),
+  auth: {
+    user: process.env.EMAIL_SERVER_USER,
+    pass: process.env.EMAIL_SERVER_PASSWORD,
+  },
+});
+
+const sendVerificationEmail = async (to: string, signInUrl: string) => {
+  await transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject: "Sign in to My App",
+    text: `Sign in to the app by clicking on this link: ${signInUrl}`,
+    html: `<p>Click the link below to sign in to <strong>My App</strong>:</p>
+           <p><a href="${signInUrl}">Sign in to My App</a></p>`,
+  });
+};
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
@@ -29,6 +48,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
       allowDangerousEmailAccountLinking: true,
     }),
+    {
+      id: "nodemailer",
+      name: Nodemailer.name,
+      type: "email",
+      sendVerificationRequest: async ({ identifier: email, url }) => {
+        await sendVerificationEmail(email, url);
+      },
+    },
+
+    /*
     Nodemailer({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
@@ -40,6 +69,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
       from: process.env.EMAIL_FROM,
     }),
+    */
   ],
 
   callbacks: {
