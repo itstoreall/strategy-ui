7bbaab6a1ccb8cce78a8879dc12c40bf8e8221b5
Serhii Stanislav <serhiistanislav@gmail.com>
2024-10-21 14:04:44 +0300

    EDIT | signOut

--- src/lib/auth/signOutServerAction.ts
+++ src/lib/auth/signOutServerAction.ts
@@ -2,10 +2,22 @@
 
 import { signOut } from "@/src/lib/auth/authConfig";
 
+const clearCookies = () => {
+  document.cookie =
+    "__Secure-authjs.session-token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC; Secure; SameSite=Lax";
+};
+
 export const handleSignOut = async () => {
   try {
     console.log(1, "sign out");
     await signOut();
+
+    // ---
+
+    clearCookies();
+    // window.location.href = "/auth/sign-in";
+
+    // ---
   } catch (error) {
     throw error;
   }
