dca25d8125083e938b9d9a990356c568c92b1944
Serhii Stanislav <serhiistanislav@gmail.com>
2024-10-23 15:06:04 +0300

    EDIT | NodemailerConfig type

--- src/lib/auth/authConfig.ts
+++ src/lib/auth/authConfig.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import NextAuth from "next-auth";
 import axios from "axios";
 import Google from "next-auth/providers/google";
-import Nodemailer from "next-auth/providers/nodemailer";
+import Nodemailer, { NodemailerConfig } from "next-auth/providers/nodemailer";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { createTransport } from "nodemailer";
 import { prisma } from "@/src/lib/prisma/client";
@@ -10,20 +11,70 @@ import { error } from "console";
 
 const strategyApiUrl = process.env.NEXT_PUBLIC_STRATEGY_API_URL;
 
-const SMTPFrom = process.env.EMAIL_FROM;
-
-const SMTPCreds = {
+/*
+const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SERVER_HOST,
   port: parseInt(process.env.EMAIL_SERVER_PORT!, 10),
   auth: {
     user: process.env.EMAIL_SERVER_USER,
     pass: process.env.EMAIL_SERVER_PASSWORD,
   },
+});
+
+function generateRandomNumbers() {
+  let verificationCode = "";
+  for (let i = 0; i < 4; i++) {
+    verificationCode += Math.floor(Math.random() * 10);
+  }
+  return verificationCode;
+}
+
+type Args = {
+  identifier: string;
+  url: string;
 };
+
+const sendVerificationEmail = async ({ identifier, url }: Args) => {
+  const code = generateRandomNumbers();
+  console.log(1, "strategyApiUrl", strategyApiUrl);
+  const payload = { identifier, code, url };
+  await axios.post(`${strategyApiUrl}/api/user/verify/code`, payload);
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to: identifier,
+    subject: "Sign in to Strategy",
+    text: `Sign in to the app`,
+    html: `<p>Code:</p>
+           <p><strong>${code}</strong></p>`,
+  });
+};
+*/
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_SERVER_HOST,
+//   port: parseInt(process.env.EMAIL_SERVER_PORT!, 10),
+//   auth: {
+//     user: process.env.EMAIL_SERVER_USER,
+//     pass: process.env.EMAIL_SERVER_PASSWORD,
+//   },
+// });
 
 type VerifyReqParams = {
   identifier: string;
   url: string;
+  provider: NodemailerConfig;
+  // provider: {
+  //   server: {
+  //     host: string;
+  //     port: number;
+  //     auth: {
+  //       user: string;
+  //       pass: string;
+  //     };
+  //   };
+  // from: string;
+  // };
 };
 
 const generateRandomNumbers = () => {
@@ -35,9 +86,9 @@ const generateRandomNumbers = () => {
 };
 
 export async function sendVerificationRequest(params: VerifyReqParams) {
-  const { identifier, url } = params;
+  const { identifier, url, provider } = params;
   const { host } = new URL(url);
-  const transport = createTransport(SMTPCreds);
+  const transport = createTransport(provider.server);
 
   const code = generateRandomNumbers();
   const payload = { identifier, code, url };
@@ -46,7 +97,7 @@ export async function sendVerificationRequest(params: VerifyReqParams) {
   try {
     await transport.sendMail({
       to: identifier,
-      from: SMTPFrom,
+      from: provider.from,
       subject: `Sign in to ${host}`,
       text: text({ url, host }),
       html: html({ url, host, code }),
@@ -115,10 +166,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
     // /*
     Nodemailer({
-      server: SMTPCreds,
-      from: SMTPFrom,
-      sendVerificationRequest({ identifier, url }) {
-        sendVerificationRequest({ identifier, url });
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: parseInt(process.env.EMAIL_SERVER_PORT!, 10),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      sendVerificationRequest({ identifier, url, provider }) {
+        sendVerificationRequest({ identifier, url, provider });
       },
     }),
     // */
