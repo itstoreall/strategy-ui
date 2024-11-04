3e5fed1ca9a28d30453945504a34077dc3f7d32c
Serhii Stanislav <serhiistanislav@gmail.com>
2024-11-04 17:48:03 +0200

    ADD | test scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next';
 import Providers from '../providers';
 import '@/src/sass/globals.scss';
 
-type ChildrenProps = {
-  children: React.ReactNode;
-};
+// type ChildrenProps = {
+//   children: React.ReactNode;
+// };
 
 export const metadata: Metadata = {
   manifest: '/manifest.json',
@@ -12,11 +12,19 @@ export const metadata: Metadata = {
   description: 'Crypto market strategies',
 };
 
-export default function RootLayout({ children }: ChildrenProps) {
+// export default function RootLayout({ children }: ChildrenProps) {
+export default function RootLayout() {
   return (
     <html lang="en">
       <Providers>
-        <body>{children}</body>
+        <body>
+          <ul>
+            {[...Array(120)].map((_, index) => (
+              <li key={index}>{index + 1}</li>
+            ))}
+          </ul>
+        </body>
+        {/* <body>{children}</body> */}
       </Providers>
     </html>
   );
