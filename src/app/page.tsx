6ec4cf1bad9591da12ba6a94317f6804b99746f7
Serhii Stanislav <serhiistanislav@gmail.com>
2024-11-05 17:06:08 +0200

    ADD | Home page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
   return (
     <PageWrapper>
       <div className="home-page">
-        <Logo className="home-page-logo" iconSize={'3rem'} />
+        <Logo className="home-page-logo" iconSize={'4rem'} />
         <Navigation session={session} className={'home-page-nav'} />
       </div>
     </PageWrapper>
