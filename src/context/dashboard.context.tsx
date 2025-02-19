f6e52df9f5f68b3e7a6c4f61eb5505856fe03229
Serhii Stanislav <serhiistanislav@gmail.com>
2025-02-19 17:28:32 +0200

    ADD | init DashboardContext

--- src/context/dashboard.context.tsx
+++ src/context/dashboard.context.tsx
@@ -5,30 +5,26 @@ import { ChildrenProps, StrategyOrders, Token } from '@/src/types';
 import useFetchAllUserOrders from '@/src/hooks/order/useFetchAllUserOrders';
 import useFetchAllTokens from '@/src/hooks/token/useFetchAllTokens';
 
-export type ModalContextProps = {
+export type DashboardContextProps = {
   userId: string | null;
   updatedTokens: Token[] | null;
   userOrders: StrategyOrders | null;
 };
 
-const initContext: ModalContextProps = {
+const initContext: DashboardContextProps = {
   userId: '',
   updatedTokens: null,
   userOrders: null,
 };
 
-const DashboardContext = createContext<ModalContextProps>(initContext);
+const DashboardContext = createContext<DashboardContextProps>(initContext);
 
 export const DashboardProvider = ({ children }: ChildrenProps) => {
   const { data: session } = useSession();
-
   const userId = session?.user?.id || null;
 
   const { userOrders } = useFetchAllUserOrders(userId, { enabled: !!userId });
   const { updatedTokens } = useFetchAllTokens();
-
-  // console.log('tok 1:', updatedTokens);
-  // console.log('ord 1:', userOrders);
 
   const values = useMemo(() => {
     return {
