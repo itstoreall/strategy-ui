bb5471a9273447437089f13b6ccc5220320fcede
Serhii Stanislav <serhiistanislav@gmail.com>
2025-04-23 14:46:55 +0300

    EDIT | code refactoring

--- src/components/Section/Chart/ChartSection.tsx
+++ src/components/Section/Chart/ChartSection.tsx
@@ -10,7 +10,8 @@ type Interval = {
   value: ChartIntervalEnum;
 };
 
-const config = {
+const c = {
+  others: 'OTHERS',
   gold: 'GOLD',
   spx500: 'S&P500',
   nasdaq: 'Nasdaq',
@@ -31,45 +32,45 @@ const config = {
 // Symbols:
 const btcD = { label: Symbol.BitcoinDominance, value: Symbol.BitcoinDominance };
 const usdtD = { label: Symbol.TetherDominance, value: Symbol.TetherDominance };
-const gold = { label: config.gold, value: Symbol.Gold };
-const spx500 = { label: config.spx500, value: Symbol.SPX500 };
-const nasdaq = { label: config.nasdaq, value: Symbol.Nasdaq };
-const m2 = { label: config.m2, value: Symbol.M2LiquidityFRED };
-const dxy = { label: config.dxy, value: Symbol.DXY };
+const gold = { label: c.gold, value: Symbol.Gold };
+const spx500 = { label: c.spx500, value: Symbol.SPX500 };
+const nasdaq = { label: c.nasdaq, value: Symbol.Nasdaq };
+const m2 = { label: c.m2, value: Symbol.M2LiquidityFRED };
+const dxy = { label: c.dxy, value: Symbol.DXY };
 const btcEth = { label: Symbol.BitcoinEther, value: Symbol.BitcoinEther };
 const ethBtc = { label: Symbol.EtherBitcoin, value: Symbol.EtherBitcoin };
-const others = { label: Symbol.OthersBitcoin, value: Symbol.OthersBitcoin };
+const others = { label: c.others, value: Symbol.OthersBitcoin };
 const total = { label: Symbol.Total, value: Symbol.Total };
 const total2 = { label: Symbol.Total2, value: Symbol.Total2 };
 const total3 = { label: Symbol.Total3, value: Symbol.Total3 };
-const yena = { label: config.yena, value: Symbol.Yena };
-const yuan = { label: config.yuan, value: Symbol.Yuan };
-const wti = { label: config.wti, value: Symbol.OilWTI };
-const brent = { label: config.brent, value: Symbol.OilBrent };
+const yena = { label: c.yena, value: Symbol.Yena };
+const yuan = { label: c.yuan, value: Symbol.Yuan };
+const wti = { label: c.wti, value: Symbol.OilWTI };
+const brent = { label: c.brent, value: Symbol.OilBrent };
 
 // Intervals:
-const hour = { label: config.hour, value: ChartIntervalEnum.Hour };
-const day = { label: config.day, value: ChartIntervalEnum.Day };
-const week = { label: config.week, value: ChartIntervalEnum.Week };
-const month = { label: config.month, value: ChartIntervalEnum.Month };
+const hour = { label: c.hour, value: ChartIntervalEnum.Hour };
+const day = { label: c.day, value: ChartIntervalEnum.Day };
+const week = { label: c.week, value: ChartIntervalEnum.Week };
+const month = { label: c.month, value: ChartIntervalEnum.Month };
 
 const symbols = [
   btcD,
-  usdtD,
-  gold,
-  spx500,
-  others,
-  m2,
   dxy,
+  total3,
+  others,
+  spx500,
+  gold,
+  m2,
+  brent,
   btcEth,
   ethBtc,
+  usdtD,
   total,
   total2,
-  total3,
   yena,
   yuan,
   wti,
-  brent,
   nasdaq,
 ];
 
@@ -118,7 +119,7 @@ const ChartSection = () => {
               .filter((opt) => opt.value !== symbol)
               .map((opt) => opt.label)}
             initialOption={symbols.find((opt) => opt.value === symbol)?.label}
-            placeholder={config.symbolPlaceholder}
+            placeholder={c.symbolPlaceholder}
             onSelect={(label) => {
               const selectedOption = symbols.find((opt) => opt.label === label);
               if (selectedOption) setSymbol(selectedOption.value);
@@ -134,7 +135,7 @@ const ChartSection = () => {
             initialOption={
               intervals.find((opt: Interval) => opt.value === interval)?.label
             }
-            placeholder={config.symbolInterval}
+            placeholder={c.symbolInterval}
             onSelect={(label) => handleInterval(label as ChartIntervalEnum)}
             isOpen={openDropdownId === interval}
             onToggle={() => toggleDropdown(interval)}
@@ -149,107 +150,3 @@ const ChartSection = () => {
 };
 
 export default ChartSection;
-
-/*
-import { useEffect, useState } from 'react';
-import useSelectMulti from '@/src/hooks/useSelectMulti';
-import { ChartSymbolEnum as Symbol, ChartIntervalEnum } from '@/src/enums';
-import TradingViewWidget from '@/src/components/Section/Chart/TradingViewWidget';
-import SelectMulti from '@/src/components/Form/SelectMulti';
-
-const symbolOptions = [
-  { label: Symbol.BitcoinDominance, value: Symbol.BitcoinDominance },
-  { label: Symbol.TetherDominance, value: Symbol.TetherDominance },
-  { label: 'GOLD', value: Symbol.Gold },
-  { label: 'S&P500', value: Symbol.SPX500 },
-  { label: 'Nasdaq', value: Symbol.Nasdaq },
-  { label: 'M2', value: Symbol.M2LiquidityFRED },
-  { label: Symbol.BitcoinEther, value: Symbol.BitcoinEther },
-  { label: Symbol.EtherBitcoin, value: Symbol.EtherBitcoin },
-  { label: Symbol.OthersBitcoin, value: Symbol.OthersBitcoin },
-  { label: Symbol.Total, value: Symbol.Total },
-  { label: Symbol.Total2, value: Symbol.Total2 },
-  { label: Symbol.Total3, value: Symbol.Total3 },
-  { label: 'Yena', value: Symbol.Yena },
-  { label: 'Yuan', value: Symbol.Yuan },
-  { label: 'Oil WTI', value: Symbol.OilWTI },
-  { label: 'Oil Brent', value: Symbol.OilBrent },
-  { label: 'DXY', value: Symbol.DXY },
-];
-
-const intervalOptions = [
-  { label: 'Hour', value: ChartIntervalEnum.Hour },
-  { label: 'Day', value: ChartIntervalEnum.Day },
-  { label: 'Week', value: ChartIntervalEnum.Week },
-  { label: 'Month', value: ChartIntervalEnum.Month },
-];
-
-const ChartSection = () => {
-  const [chartSymbol, setChartSymbol] = useState(symbolOptions[0].value);
-  const [chartInterval, setChartInterval] = useState(intervalOptions[1].value);
-
-  const { openDropdownId, toggleDropdown } = useSelectMulti();
-
-  const isM2 = chartSymbol === Symbol.M2LiquidityFRED;
-
-  useEffect(() => {
-    if (isM2) {
-      setChartInterval(ChartIntervalEnum.Month);
-    }
-  }, [isM2]);
-
-  // ---
-
-  const handleInterval = (label: ChartIntervalEnum) => {
-    const selectedOption = intervalOptions.find((opt) => opt.label === label);
-    if (selectedOption) setChartInterval(selectedOption.value);
-  };
-
-  return (
-    <section className="section chart">
-      <div className="chart-section-content">
-        <div className="chart-section-select-wrapper">
-          <SelectMulti
-            options={symbolOptions
-              .filter((opt) => opt.value !== chartSymbol)
-              .map((opt) => opt.label)}
-            initialOption={
-              symbolOptions.find((opt) => opt.value === chartSymbol)?.label
-            }
-            placeholder="Symbol"
-            onSelect={(label) => {
-              const selectedOption = symbolOptions.find(
-                (opt) => opt.label === label
-              );
-              if (selectedOption) setChartSymbol(selectedOption.value);
-            }}
-            isOpen={openDropdownId === chartSymbol}
-            onToggle={() => toggleDropdown(chartSymbol)}
-          />
-
-          <SelectMulti
-            options={intervalOptions
-              .filter((opt) => opt.value !== chartInterval)
-              .map((opt) => opt.label)}
-            initialOption={
-              intervalOptions.find((opt) => opt.value === chartInterval)?.label
-            }
-            placeholder="Interval"
-            onSelect={(label) => handleInterval(label as ChartIntervalEnum)}
-            isOpen={openDropdownId === chartInterval}
-            onToggle={() => toggleDropdown(chartInterval)}
-            isDisable={isM2}
-          />
-        </div>
-
-        <TradingViewWidget
-          chartSymbol={chartSymbol}
-          chartInterval={chartInterval}
-        />
-      </div>
-    </section>
-  );
-};
-
-export default ChartSection;
-*/
--- src/context/global.context.tsx
+++ src/context/global.context.tsx
@@ -10,7 +10,7 @@ import * as t from '@/src/types';
 type SortTokens = (a: t.Token, b: t.Token) => number;
 
 const config = {
-  appVersion: 'v1.4.15',
+  appVersion: 'v1.4.16',
   dashboardPath: '/dashboard',
   strategyPath: '/strategy/',
   fetchTokens: 'Fetch was successful:',
