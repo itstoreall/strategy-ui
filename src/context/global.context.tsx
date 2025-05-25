9379d2b54768df58fb070bc59802a59336bfc845
Serhii Stanislav <serhiistanislav@gmail.com>
2025-05-25 16:36:36 +0300

    EDIT | Heading button style

--- src/context/global.context.tsx
+++ src/context/global.context.tsx
@@ -10,7 +10,7 @@ import * as t from '@/src/types';
 type SortTokens = (a: t.Token, b: t.Token) => number;
 
 const config = {
-  appVersion: 'v1.4.73',
+  appVersion: 'v1.4.74',
   dashboardPath: '/dashboard',
   strategyPath: '/strategy/',
   fetchTokens: 'Fetch was successful:',
