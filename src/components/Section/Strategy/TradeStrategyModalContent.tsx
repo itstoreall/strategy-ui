65cd1c8b75cb5d3f54b89ad8871853ee5726113f
Serhii Stanislav <serhiistanislav@gmail.com>
2025-06-01 21:16:55 +0300

    EDIT | code refactoring

--- src/components/Section/Strategy/TradeStrategyModalContent.tsx
+++ src/components/Section/Strategy/TradeStrategyModalContent.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { TradeStrategy } from '@/src/types';
 import { uniNumberFormatter, normalizeKyivDate } from '@/src/utils';
 import * as sec from '@/src/components/Section/Strategy/TradeStrategySection';
+import DefaultInput from '@/src/components/Form/DefaultInput';
 import Button from '@/src/components/Button/Button';
 
 type Props = {
@@ -12,30 +15,81 @@ type Props = {
   deleteHystory: () => void;
 };
 
+type StringState = Dispatch<SetStateAction<string>>;
+
+const c = {
+  amount: 'amount',
+  buy: 'buy',
+  sell: 'sell',
+};
+
 const TradeStrategyModalContent = (props: Props) => {
+  const [amount, setAmount] = useState('');
+  const [buyPrice, setBuyPrice] = useState('');
+  const [sellPrice, setSellPrice] = useState('');
+
   const {
-    strategyHistory,
+    // strategyHistory,
     storedStrategy,
-    updateStrategyHistory,
+    // updateStrategyHistory,
     createNewBuyTarget,
     resetTradeStrategy,
-    deleteHystory,
+    // deleteHystory,
   } = props;
+
+  useEffect(() => {
+    if (storedStrategy) {
+      setAmount(storedStrategy.amount.toString());
+      setBuyPrice(storedStrategy.avgBuyPrice.toString());
+      setSellPrice(storedStrategy.sellPrice.toString());
+    }
+  }, [storedStrategy]);
+
+  useEffect(() => {
+    if (storedStrategy && !amount) {
+      setAmount(storedStrategy.amount.toString());
+    }
+  }, [amount]);
+
+  useEffect(() => {
+    if (storedStrategy && !buyPrice) {
+      setBuyPrice(storedStrategy.avgBuyPrice.toString());
+    }
+  }, [buyPrice]);
+
+  useEffect(() => {
+    if (storedStrategy && !sellPrice) {
+      setSellPrice(storedStrategy.sellPrice.toString());
+    }
+  }, [sellPrice]);
+
+  const handleNumericInput = (val: string, state: StringState | null) => {
+    if (!state) return;
+    let value = val.replace(/,/g, '.');
+    if (/^\d*\.?\d*$/.test(value)) {
+      if (value.startsWith('.')) {
+        value = `0${value}`;
+      }
+      state(value);
+    } else {
+      state(value.slice(0, -1));
+    }
+  };
 
   const NewEntry = ({ strategy }: { strategy: TradeStrategy }) => {
     const isAVGPrice = strategy.orders.split(', ').length > 1;
 
-    const buyPrice = isAVGPrice
-      ? uniNumberFormatter(strategy.avgBuyPrice)
-      : strategy.avgBuyPrice;
+    const formatedBuyPrice = isAVGPrice
+      ? uniNumberFormatter(+buyPrice)
+      : buyPrice;
 
     const newEntry = [
       { name: 'date', value: normalizeKyivDate(strategy.date) },
       { name: 'symbol', value: strategy.symbol },
       { name: 'exchange', value: strategy.exchange },
-      { name: 'amount', value: strategy.amount },
-      { name: 'buy price (AVG)', value: buyPrice },
-      { name: 'sell price', value: strategy.sellPrice },
+      { name: c.amount, value: amount },
+      { name: c.buy, value: formatedBuyPrice },
+      { name: c.sell, value: sellPrice },
       { name: 'invested', value: strategy.invested },
       { name: 'profit', value: strategy.profit },
       { name: 'total', value: uniNumberFormatter(strategy.total) },
@@ -44,14 +98,18 @@ const TradeStrategyModalContent = (props: Props) => {
 
     return storedStrategy ? (
       <ul className="new-history-entry-list">
-        {newEntry.map((el, idx) => (
-          <li key={idx} className="new-history-entry-list-item">
-            <span>{`${el.name}:`}</span>
-            <span>
-              {el.name === 'profit' ? uniNumberFormatter(+el.value) : el.value}
-            </span>
-          </li>
-        ))}
+        {newEntry.map((el, idx) => {
+          return (
+            <li key={idx} className="new-history-entry-list-item">
+              <span>{`${el.name}:`}</span>
+              <span>
+                {el.name === 'profit'
+                  ? uniNumberFormatter(+el.value)
+                  : el.value}
+              </span>
+            </li>
+          );
+        })}
       </ul>
     ) : (
       <div>No Trade Strategy</div>
@@ -63,13 +121,28 @@ const TradeStrategyModalContent = (props: Props) => {
       <div className="trade-strategy-modal-new-history-entry-block"></div>
       {storedStrategy && <NewEntry strategy={storedStrategy} />}
 
-      <Button
+      <div>
+        <DefaultInput
+          value={amount}
+          handleChange={(val) => handleNumericInput(val, setAmount)}
+        />
+        <DefaultInput
+          value={buyPrice}
+          handleChange={(val: string) => handleNumericInput(val, setBuyPrice)}
+        />
+        <DefaultInput
+          value={sellPrice}
+          handleChange={(val: string) => handleNumericInput(val, setSellPrice)}
+        />
+      </div>
+
+      {/* <Button
         style={{ marginBottom: '1rem' }}
         clickContent={updateStrategyHistory}
         disabled={!storedStrategy}
       >
         Save
-      </Button>
+      </Button> */}
       <Button
         style={{ marginBottom: '1rem' }}
         clickContent={createNewBuyTarget}
@@ -84,11 +157,11 @@ const TradeStrategyModalContent = (props: Props) => {
       >
         Clear Storage
       </Button>
-      <Button clickContent={deleteHystory} disabled={!strategyHistory}>
+      {/* <Button clickContent={deleteHystory} disabled={!strategyHistory}>
         Delete Hystory
-      </Button>
+      </Button> */}
 
-      {strategyHistory ? (
+      {/* {strategyHistory ? (
         <ul className="trade-strategy-modal-history-list">
           {strategyHistory.map((el, idx) => (
             <li key={idx} className="trade-strategy-modal-history-list-item">
@@ -101,7 +174,7 @@ const TradeStrategyModalContent = (props: Props) => {
         </ul>
       ) : (
         <div>No History</div>
-      )}
+      )} */}
     </>
   );
 };
