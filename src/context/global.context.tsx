ed6aa23d26a4e52c9b68db15965c85097f6b5651
Serhii Stanislav <serhiistanislav@gmail.com>
2025-06-18 16:20:16 +0300

    EDIT | authConfig

--- src/context/global.context.tsx
+++ src/context/global.context.tsx
@@ -10,7 +10,7 @@ import * as t from '@/src/types';
 type SortTokens = (a: t.Token, b: t.Token) => number;
 
 const c = {
-  appVersion: 'v1.5.20',
+  appVersion: 'v1.5.21',
   adminPath: '/admin',
   chartPath: '/chart',
   dashboardPath: '/dashboard',
@@ -63,26 +63,21 @@ export const GlobalProvider = ({ children }: t.ChildrenProps & {}) => {
 
   const userId = session?.user?.id || null;
   const app = { version: c.appVersion };
-  const isChart = path === c.chartPath;
   const isDashboard = path === c.dashboardPath;
   const isStrategy = path.includes(c.strategyPath);
 
   useEffect(() => {
-    if (isDashboard || isStrategy) {
-      setTimeout(() => {
-        if (updatedTokens === null) {
-          fetchTokens();
-        }
-      }, c.initDelay);
-    }
+    setTimeout(() => {
+      if (updatedTokens === null) {
+        fetchTokens();
+      }
+    }, c.initDelay);
 
     // Fear and Greed
-    if (isChart) {
-      chartService.fetchFearAndGreedIndex().then((idx) => {
-        if (idx) setFearAndGreed(idx);
-      });
-    }
-  }, [isChart, isDashboard, isStrategy]);
+    chartService.fetchFearAndGreedIndex().then((idx) => {
+      if (idx) setFearAndGreed(idx);
+    });
+  }, []);
 
   useEffect(() => {
     // if (window.location.hostname === 'localhost') return;
