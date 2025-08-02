31be46d0cd2d3f3c5a71a498de75fb4cf3e989d5
Serhii Stanislav <serhiistanislav@gmail.com>
2025-08-02 14:30:04 +0200

    EDIT | DCAP status logic

--- src/context/global.context.tsx
+++ src/context/global.context.tsx
@@ -12,7 +12,7 @@ import * as t from '@/src/types';
 type SortTokens = (a: t.Token, b: t.Token) => number;
 
 const c = {
-  appVersion: 'v1.5.83',
+  appVersion: 'v1.5.84',
   adminPath: '/admin',
   chartPath: '/chart',
   dashboardPath: '/dashboard',
