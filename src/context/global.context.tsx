e412835102d9e2645c5a6ff5ef55e0c6e173008f
Serhii Stanislav <serhiistanislav@gmail.com>
2025-10-04 21:24:26 +0300

    EDIT | BITUNIX style

--- src/context/global.context.tsx
+++ src/context/global.context.tsx
@@ -12,7 +12,7 @@ import * as t from '@/src/types';
 type SortTokens = (a: t.Token, b: t.Token) => number;
 
 const c = {
-  appVersion: 'v1.6.19',
+  appVersion: 'v1.6.20',
   localhost: 'localhost',
   adminPath: '/admin',
   chartPath: '/chart',
